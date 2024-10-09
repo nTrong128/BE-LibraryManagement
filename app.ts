199c60ed671f6ca2269e1bd9574e127a6aa79a42
ntrong128 <lenhattrong128@gmail.com>
2024-10-10 02:13:09 +0700

    update schema and add cookieParser

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import {errorHandler} from "./middlewares/errorHandler";
-
+import cookieParser from "cookie-parser";
 import {router} from "./routes";
 
 dotenv.config();
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 // Routes
