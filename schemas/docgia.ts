e285879a648d249c1ee7026b5cff6c01ac53d29f
ntrong128 <lenhattrong128@gmail.com>
2024-10-09 07:27:56 +0700

    add muonsach schema

--- schemas/docgia.ts
+++ schemas/docgia.ts
@@ -5,8 +5,12 @@ export const DocGiaSchema = z.object({
   HoLot: z.string().min(1, {message: "Họ lót không được để trống"}),
   Ten: z.string().min(1, {message: "Tên không được để trống"}),
   DiaChi: z.string().min(1, {message: "Địa chỉ không được để trống"}),
-  DienThoai: z.string().regex(/^0\d{9,10}$/),
-  Phai: z.boolean(),
+  DienThoai: z.string().regex(/^0\d{9,10}$/, {
+    message: "Số điện thoại phải bắt đầu bằng số 0 và có 10 hoặc 11 chữ số",
+  }),
+  Phai: z.enum(["MALE", "FEMALE"], {
+    message: "Giới tính không hợp lệ",
+  }),
   NgaySinh: z.coerce.date(),
 });
 
