53d2f37fbccc5c7e6da24f0ab0d8bc26db97bd92
ntrong128 <lenhattrong128@gmail.com>
2024-10-12 09:11:33 +0700

    modify schema and config cors

--- app.ts
+++ app.ts
@@ -12,7 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
